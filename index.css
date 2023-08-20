7d2b88cca8251585b9f779ade207c51f7863df40
xmalone0920 <xmalone0920@gmail.com>
2023-08-20 13:39:36 -0700

    done

---
+++ index.css
@@ -0,0 +1,64 @@
+body {
+    display: inline-grid;
+    grid-template-rows: 1fr 3fr 1fr;
+    grid-template-columns: 3fr 2fr;
+    row-gap: 20px;
+    column-gap: 20px;
+}  
+
+header {
+    grid-row: 1;
+    grid-column-start: 1;
+    grid-column-end: 4;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: rgb(215, 107, 187);
+}
+
+div {
+    display: flex;
+    grid-row: 2;
+    grid-column-start: 1;
+    grid-column-end: 3;
+}
+
+
+div.header {
+
+}
+
+div.article {
+    flex-direction: column;
+}
+
+
+aside {
+    grid-row: 2;
+    grid-column-start: 3;
+    grid-column-end: 4;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+   background-color: rgb(215, 107, 187);
+}
+
+
+footer {
+    grid-column: 1/3;
+    grid-row: 3;
+    display: flex;
+    align-items: center;
+}
+div.list {
+    display: flex;
+    flex-direction: column;
+    column-gap: 20px;
+
+}
+h1 {
+    display: flex;
+    justify-content: center;
+}
